242b87648c648fc9610a77f6e5335c9ee3f6f809
Knapoj Chaimanekorn <63010104@kmitl.ac.th>
2021-07-21 17:19:59 +0700

    Bug fixing

--- charmschool/utils.py
+++ charmschool/utils.py
@@ -2,10 +2,11 @@ from django.core.exceptions import PermissionDenied
 
 def check_group_visiblilty(content, user):
     for group in user.groups.all():
-        if group in content.visible_by.all():
-            break
-        raise PermissionDenied
-    return True
+        for content_group in content.visible_by.all():
+            if group == content_group:
+                return True
+    raise PermissionDenied
+    
 def check_access_permission(group,user):
     if not group in user.groups.all():
         raise PermissionDenied
